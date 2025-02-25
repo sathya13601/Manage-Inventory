234ce308e190c58528fc08e20be66fab2d0d036c
sathya13601 <ext.sathyareddy13601@xpressbees.com>
2025-02-25 12:01:25 +0530

    Add files via upload

---
+++ ManageInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.manage.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageInventoryApplication.class, args);
+	}
+
+}
